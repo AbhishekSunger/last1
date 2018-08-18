d387270df17691d1c1a04cbf0ebf4ad3caef5ff6
SURYA KUMAR SUNGER <>
2018-08-18 19:52:58 +0530

    blue color

---
+++ Last/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Last
+//
+//  Created by SURYA KUMAR SUNGER on 18/08/18.
+//  Copyright © 2018 Antispol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.blue
+        
+        
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
